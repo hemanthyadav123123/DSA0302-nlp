2e6dc501b432949d2dcf42a94718b58225c4a9f0
hemanthyadav123123 <128719872+hemanthyadav123123@users.noreply.github.com>
2026-08-03 09:49:01 +0530

    Update EXP 2.py

--- lab programs/EXP 2.py
+++ lab programs/EXP 2.py
@@ -1,12 +1,10 @@
-# Finite State Automaton to recognize strings ending with 'ab'
+
 
 def fsa(string):
     if string.endswith("ab"):
         print("Accepted")
     else:
         print("Rejected")
-
-# Input from user
 text = input("Enter a string: ")
 
 # Function call
