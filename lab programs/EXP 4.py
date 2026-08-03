07c96b847afb0b03718fd5755dcebb8b5f5ee90d
hemanthyadav123123 <128719872+hemanthyadav123123@users.noreply.github.com>
2026-08-03 09:58:55 +0530

    Update EXP 4.py

--- lab programs/EXP 4.py
+++ lab programs/EXP 4.py
@@ -1,6 +1,5 @@
 
 def pluralize(noun):
-    # Nouns ending with s, x, z, ch, sh
     if noun.endswith(("s", "x", "z", "ch", "sh")):
         return noun + "es"
     elif noun.endswith("y") and noun[-2].lower() not in "aeiou":
