9a89a5c534087ca29d1a1db7a98da2df2673c57d
hemanthyadav123123 <128719872+hemanthyadav123123@users.noreply.github.com>
2026-08-03 08:42:02 +0530

    Add files via upload

---
+++ ASSESSEMENT CO1 AT2/TECHNICAL PRESENTATION/TECHNICAL PRESENTATION IMPLEMENTATION (2).py
@@ -0,0 +1,10 @@
+import re
+
+email = "abc@gmail.com"
+
+pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+
+if re.match(pattern, email):
+    print("Valid Email")
+else:
+    print("Invalid Email")
